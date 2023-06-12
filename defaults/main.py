729dede3bb7c6552a51589fcc56b04f8f66546e9
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-06-12 08:17:33 +0000

    Update main.py

--- defaults/main.py
+++ defaults/main.py
@@ -2,11 +2,10 @@ import os
 
 
 mainDir = os.path.abspath (os.path.dirname (__file__))
-dependencies_file = os.path.join (mainDir, "dependencies.json")
 
 
 import setup
-setup.setup (setup.get_dependencies (dependencies_file))
+setup.main (mainDir)
 
 
 if __name__ == "__main__":
