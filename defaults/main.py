a611af9020bf50d04179e1eb9285ca12057dfa4d
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-06-23 18:48:36 +0000

    Update main.py

--- defaults/main.py
+++ defaults/main.py
@@ -6,6 +6,14 @@ import setup
 setup.main (mainDir)
 
 
+def print_error (error: str):
+    print (F"\033[1;31m[-] {error}\033[0m")
+
+
+def print_success (succ: str):
+    print (f"\033[1;32m[+] {succ}\033[0m")
+
+
 def main ():
     # write your code
     pass
