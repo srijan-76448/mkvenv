f5f65ebc4577a955c4fe0de68e075304a5c13315
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-06-23 18:15:21 +0000

    Update main.py

--- defaults/main.py
+++ defaults/main.py
@@ -1,13 +1,15 @@
 import os
 
-
 mainDir = os.path.abspath (os.path.dirname (__file__))
-
 
 import setup
 setup.main (mainDir)
 
 
-if __name__ == "__main__":
+def main ():
     # write your code
     pass
+
+
+if __name__ == "__main__":
+    main ()
