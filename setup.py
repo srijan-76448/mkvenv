5ebeb03196d7cc77f1c23b520be10b0912783147
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-08-10 22:30:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import os, sys
 
 
 def setup () -> None:
-    # Get the user's home directory
-    home_dir = os.path.expanduser ("~")
-
     # Define the alias in ~/.bashrc
     mainDir = os.path.abspath (__file__)
     python_path = sys.executable
