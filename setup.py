09e75132b73bb0c2595c21b9d906f1a2a7713e5d
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-08-10 22:35:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import os, sys
 def setup () -> None:
     # Define the alias in ~/.bashrc
     mainDir = os.path.abspath (__file__)
-    python_path = sys.executable
-    cmd = f"{python_path} {mainDir}"
+    cmd = f"/usr/bin/python3 {mainDir}"
 
     os.system (f"grep -qxF '{cmd}' ~/.bashrc || echo '{cmd}' >> ~/.bashrc")
