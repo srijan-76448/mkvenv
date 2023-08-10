120a94447ad7d01d89708582523d49e6daafc0d4
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-08-10 22:23:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import os, sys
 
 
-def setup (mainDir: os.path):
+def setup () -> None:
     # Get the user's home directory
     home_dir = os.path.expanduser ("~")
 
     # Define the alias in ~/.bashrc
+    mainDir = os.path.dirname (os.path.abspath (__file__))
     python_path = sys.executable
     cmd = f"{python_path} {mainDir}"
     os.system (f"grep -qxF '{cmd}' ~/.bashrc || echo '{cmd}' >> ~/.bashrc")
