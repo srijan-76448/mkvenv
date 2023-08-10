72b69de5eaf5d6cb4747ccd9298a93e9d24504cc
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-08-10 22:22:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,7 @@ def setup (mainDir: os.path):
     # Get the user's home directory
     home_dir = os.path.expanduser ("~")
 
-    # Define the alias
+    # Define the alias in ~/.bashrc
     python_path = sys.executable
-    command = f"{python_path} {mainDir}"
-    alias = f"alias mkvenv='{command}'"
-
-    # Build the full path to the .bashrc file
-    bashrc_path = os.path.join (home_dir, ".bashrc")
-
-
-    # Check if the alias already exists in the .bashrc file
-    with open (bashrc_path, "r") as file:
-        bashrc_content = file.read ()
-        if alias not in bashrc_content:
-            # Open the .bashrc file in append mode and write the alias
-            with open (bashrc_path, "a") as file:
-                file.write (alias + "\n")
+    cmd = f"{python_path} {mainDir}"
+    os.system (f"grep -qxF '{cmd}' ~/.bashrc || echo '{cmd}' >> ~/.bashrc")
